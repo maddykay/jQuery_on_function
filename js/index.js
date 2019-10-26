4d65d2097c94c860a698ccd37513852b2c0801ef
mthomas391 <51038116+mthomas391@users.noreply.github.com>
2019-10-26 13:40:56 -0700

    jQuery

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
-$("#button1").on("click", function(){
+$("#button1").on("click", function () {
     $(".hideMe").slideToggle();
 });
 
-$('#button2').on("click", function(){
-    $(".cssChange").css("background-color" , "red");
+$('#button2').on("click", function () {
+    $(".cssChange").css("background-color", "red");
 });
 
-$('#button3').on("click", function(){
+$('#button3').on("click", function () {
     $(".appendMe").append("I got appended. Nice!");
 });
